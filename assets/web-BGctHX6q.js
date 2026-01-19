0235d0b857fbfec8fc0aed125f416e6c85697c2b
jouke <ci@is-my-horse.app>
2026-01-19 18:09:22 +0000

    Deploy Web v0.0.7

--- assets/web-BGctHX6q.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-DxEv4TWB.js";class m extends e{async present(n){throw this.unimplemented("Not implemented on web.")}async cancel(){throw this.unimplemented("Not implemented on web.")}}export{m as DatetimePickerWeb};
